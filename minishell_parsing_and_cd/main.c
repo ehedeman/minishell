2f83b815a9038de48214faf7e4415552bd47b248
smatschu <smatschu@student.42wolfsburg.de>
2024-07-10 10:07:16 +0200

    removed env function of while loop so it doesn't reset at every readline

--- minishell_parsing_and_cd/main.c
+++ minishell_parsing_and_cd/main.c
@@ -6,12 +6,13 @@
 /*   By: smatschu <smatschu@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 10:57:19 by ehedeman          #+#    #+#             */
-/*   Updated: 2024/07/06 18:08:56 by smatschu         ###   ########.fr       */
+/*   Updated: 2024/07/10 09:54:08 by smatschu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int g_exec_file;
 //if theres multiple output redirections it creates all of the files, then
 //closes them if nessecary.
 //returns only the last fd.
@@ -45,6 +46,7 @@ int	check_redirect_output(t_mini *mini)
 int	check_command(t_mini *mini)
 {
 	t_statement *temp;
+	//t_mini		*testmini;
 	int fd;
 	int	i;
 
@@ -86,16 +88,24 @@ int	check_command(t_mini *mini)
 			}
 			else if (ft_strncmp(temp->argv[i], "env", 3) == 0 && !temp->argv[i + 1])
 				return (ft_print_env_lst(mini->env));
+			else if (ft_strncmp(temp->argv[i], "export", 6) == 0)
+			{
+				ft_export(mini);
+				// printf("LIST AFTER EXPORT:\n");
+				// ft_print_env_lst(mini->env);
+				//return (0);
+				//return (ft_export(mini));
+			}
 			else if (ft_strncmp(temp->argv[i], "rm", 2) == 0)
 			{
 				ft_rm(temp);
 				return (0);
 			}
-			else if (temp->operator != SKIP)
-			{
-				write(1, "Command not found.\n", 19);
-				return (0);
-			}
+			// else if (temp->operator != SKIP)
+			// {
+			// 	write(1, "Command not found.\n", 19);
+			// 	return (0);
+			// }
 			i++;
 		}
 		temp = mini->temp;
@@ -103,20 +113,36 @@ int	check_command(t_mini *mini)
 			close(fd);
 		temp = temp->next;
 	}
+	// printf("\n\nLIST BEFORE RETURNING FROM CHECK_COM:\n");
+	// ft_print_env_lst(mini->env);
 	return (0);
 }
+
+void	handler(int sig)
+{
+	if (sig == SIGINT && !g_exec_file)
+		printf("\nminishell: ");
+	else if (sig == SIGINT && g_exec_file) //had issues with closing endless programs (usually you use ctrl c)
+		printf("\n");					   //without else if the programm displays "minishell: " twice in that case
+}										   //used global var to tell the program "hey theres smth else beign closed with ctrl c right now"
+										   //so theres no need to display the new prompt cuz minishell does that anyways
 
 int main (int argc, char **argv, char **envp)
 {
 	t_mini	mini;
 	(void)argv;
 	(void)argc;
-	
+
+	signal(SIGINT, handler);
+	signal(SIGQUIT, SIG_IGN);
+	mini.com_tab = NULL;
+	mini.input = NULL;
+	g_exec_file = 0;
+	ft_copy_env2lst(&mini, envp); //took out of the while(1) so it doesnt reset at every readline
 	while (1)
 	{
 		mini.input = readline("minishell: ");
 
-		ft_copy_env2lst(&mini, envp);
 		if (!mini.input)
 		{
 			mini.com_tab = NULL;
@@ -129,16 +155,21 @@ int main (int argc, char **argv, char **envp)
 				add_history(mini.input);
 				mini.com_tab = parsing(mini.input, 0 , 0);
 				if (!mini.com_tab)
+				{
+					ft_env_lst_clear(mini.env, free);
 					return (0);
+				}
 				if (check_command(&mini) == -1)
 				{
 					free_com_tab(&mini);
+					ft_env_lst_clear(mini.env, free);
 					return (0);
 				}
 				if (mini.com_tab)
 					free_com_tab(&mini);
 			}
 		}
+		ft_env_lst_clear(mini.env, free);
 	}
 	return (0);
 }
