e819a538cad8e8c1c4fc91e42e6570416684577c
smatschu <smatschu@student.42wolfsburg.de>
2024-07-10 10:05:13 +0200

    add export.c and organized the env/export functions in more files for norm

--- minishell_parsing_and_cd/env_list_utils.c
+++ minishell_parsing_and_cd/env_list_utils.c
@@ -1,74 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env.c                                              :+:      :+:    :+:   */
+/*   env_list_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smatschu <smatschu@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:46:10 by smatschu          #+#    #+#             */
-/*   Updated: 2024/07/08 22:30:37 by smatschu         ###   ########.fr       */
+/*   Updated: 2024/07/09 20:20:59 by smatschu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-//this will print the linked list
-int	ft_print_env_lst(t_env_list *env)
-{
-	t_env_list	*node;
-
-	node = env;
-	if (node == NULL)
-		return (1);
-	while (node)
-	{
-		printf("%s=%s\n", node->name, node->value);
-		node = node->next;
-	}
-	return (0);
-}
-
-//this will create a new node for the linked list
-t_env_list	*ft_create_node(char *env_var)
-{
-	t_env_list	*new_node;
-	int			i;
-	int			len;
-	char		**kv;
-	
-	kv = malloc(sizeof(char *) * 3);
-	i = 0;
-	len = ft_strlen(env_var);
-	while (env_var[i] && env_var[i] != '=')
-		i++;
-	kv[0] = ft_substr(env_var, 0, i);
-	kv[1] = ft_substr(env_var, i + 1, len);
-	kv[2] =  NULL;
-	new_node = ft_env_lst_new(kv[0], kv[1]);
-	free(kv[0]);
-	free(kv[1]);
-	free(kv[2]);
-	free(kv);
-	return (new_node);
-}
-
-//this will create a linked list with the name and value of the variables in envp (t_env_list *env)
-void	ft_copy_env2lst(t_mini *mini, char **envp)
-{
-	int			i;
-	t_env_list	*node;
-
-	i = 0;
-	mini->env = NULL;
-	while (envp[i] != NULL)
-	{
-		node = ft_create_node(envp[i]);
-		if (!node)
-			perror("Error: could not create node for env list");
-		ft_env_lst_addback(&mini->env, node);
-		i++;
-	}
-}
 
 //this will create a new node for the linked list
 t_env_list *ft_env_lst_new(char *key, char *value)
@@ -81,6 +23,8 @@ t_env_list *ft_env_lst_new(char *key, char *value)
 	node->name = ft_strdup(key);
 	node->value = ft_strdup(value);
 	node->next = NULL;
+	free(key);
+	free(value);
 	return (node);
 }
 
@@ -107,14 +51,6 @@ void	ft_env_lst_addback(t_env_list **lst, t_env_list *new)
 	}
 }
 
-//this will free one node of the linked list
-void	ft_lst_delone(t_env_list *lst, void (*del)(void *))
-{
-	del(lst->name);
-	del(lst->value);
-	free(lst);
-}
-
 //this will free the linked list (added to the end of ft_exit)
 void ft_env_lst_clear(t_env_list *lst, void (*del)(void *))
 {
@@ -128,4 +64,12 @@ void ft_env_lst_clear(t_env_list *lst, void (*del)(void *))
 		lst = node;
 	}
 	lst = NULL;
+}
+
+//this will free one node of the linked list
+void	ft_lst_delone(t_env_list *lst, void (*del)(void *))
+{
+	del(lst->name);
+	del(lst->value);
+	free(lst);
 }
---
+++ minishell_parsing_and_cd/export.c
@@ -0,0 +1,165 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smatschu <smatschu@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/06 19:14:29 by smatschu          #+#    #+#             */
+/*   Updated: 2024/07/10 09:01:57 by smatschu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_free_matrix(char **matrix)
+{
+	int	index;
+
+	index = 0;
+	while (matrix[index])
+	{
+		free(matrix[index]);
+		index++;
+	}
+	free(matrix);
+}
+
+char	**ft_join_env(char *env_var)
+{
+	int		index;
+	int		len;
+	char	**pair;
+
+	index = 0;
+	len = ft_strlen(env_var);
+	pair = malloc(sizeof(char *) * 3);
+	while (env_var[index] && env_var[index] != '=')
+		index++;
+	pair[0] = ft_substr(env_var, 0, index);
+	pair[1] = ft_substr(env_var, index + 1, len);
+	pair[2] = NULL;
+	printf("pair[0]: %s\n", pair[0]);
+	printf("pair[1]: %s\n", pair[1]);
+	printf("pair[2]: %s\n", pair[2]);
+	return (pair);
+}
+
+int	ft_env_list_comp(t_env_list *env, char **val)
+{
+	t_env_list	*temp;
+
+	temp = env;
+	if (temp == NULL)
+		return (1);
+	else if (ft_strncmp(temp->name, val[0], ft_strlen(val[0])) == 0)
+	{
+		free(temp->value);
+		temp->value = ft_strdup(val[1]);
+		return (0);
+	}
+	else
+		return (ft_env_list_comp(temp->next, val));
+}
+
+void	ft_free_env(t_env_list *enval)
+{
+	free(enval->name);
+	free(enval->value);
+	free(enval);
+}
+
+void	ft_unset(t_env_list *env, char *tofind)
+{
+	t_env_list	*temp;
+
+	if (env == NULL || env->next == NULL)
+		return ;
+	temp = env;
+	if (ft_strncmp(env->next->name, tofind, ft_strlen(tofind)) == 0)
+	{
+		temp = env->next;
+		env->next = temp->next;
+		ft_free_env(temp);
+	}
+	else if (ft_strncmp(env->name, tofind, ft_strlen(tofind)) == 0)
+	{
+		temp = env;
+		env = env->next;
+		ft_free_env(temp);
+	}
+	else
+	{
+		temp = env;
+		ft_unset(temp->next, tofind);
+	}
+}
+
+int	ft_export_add(t_mini *mini, t_env_list *temp, t_env_list *new, int i)
+{
+	char	**val;
+
+	val = ft_join_env(mini->com_tab->argv[i]);
+	if (!val || !val[0]) // Check if val is NULL or val[0] is NULL
+	{
+		write(2, "export: error processing arguments\n", 35);
+		return (1);
+	}
+	if (ft_isalpha(val[0][0]) == 1)
+	{
+		if (ft_env_list_comp(mini->env, val) != 0)
+		{
+			printf("val[0]: %s\n", val[0]);
+			printf("val[1]: %s\n", val[1]);
+			new = ft_env_lst_new(val[0], val[1]);
+			if(!new)
+			{
+				ft_free_matrix(val);
+				printf("new is NULL\n");
+				return (1);
+			}
+			ft_env_lst_addback(&temp, new);
+			printf("NODE SHOULD BE NOW ADDED TO THE BACK OF LIST\n");
+			//ft_print_env_lst(temp);
+		}
+		ft_free_matrix(val);
+	}
+	else
+	{
+		write(2, "export: not a valid identifier\n", 31);
+		ft_free_matrix(val);
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_export(t_mini *mini)
+{
+	t_env_list	*temp;
+	t_env_list	*new;
+	int			i;
+
+	i = 0;
+	new = NULL;
+	temp = mini->env; // Use the address of the list head
+	if (!mini->com_tab->argv[1])
+	{
+		while (temp)
+		{
+			ft_printf("declare -x %s=\"%s\"\n", temp->name, temp->value);
+			temp = temp->next;
+		}
+	}
+	else
+	{
+		while (mini->com_tab->argv[++i] != NULL)
+		{
+			printf("mini->com_tab->argv[%d]: %s\n", i, mini->com_tab->argv[i]);
+			if(ft_export_add(mini, temp, new, i) == 1)
+				return (1);
+		}
+		// printf("\n\nLIST AFTER EXPORT @ft_export\n");
+		// ft_print_env_lst(mini->env);
+	}
+	return (0);
+}
