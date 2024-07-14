713bc747d509b0521a81b5aa0e9cdb45dabd0f8e
smatschu <smatschu@student.42wolfsburg.de>
2024-07-14 16:10:09 +0200

    add print functions to their own .c file, norm ok

---
+++ minishell_parsing_and_cd/env_list_print.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_list_print.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smatschu <smatschu@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 20:14:40 by smatschu          #+#    #+#             */
+/*   Updated: 2024/07/14 15:46:07 by smatschu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+//in Bash env prints the var in the order they were defined in the environment
+int	print_env_lst(t_env_list *env)
+{
+	t_env_list	*node;
+
+	node = env;
+	if (node == NULL)
+		return (1);
+	while (node)
+	{
+		if (node->value != NULL)
+			printf("%s=%s\n", node->name, node->value);
+		node = node->next;
+	}
+	return (0);
+}
+
+//export prints in alphabetical order
+void	print_export_list(t_env_list *sorted_env)
+{
+	t_env_list	*node;
+
+	node = sorted_env;
+	while (node)
+	{
+		if (node->name[0] != '_')
+		{
+			if (node->value != NULL && ft_strcmp(node->value, "") != 0)
+				printf("declare -x %s=\"%s\"\n", node->name, node->value);
+			else if (node->value != NULL && node->value[0] == '\0')
+				printf("declare -x %s=\n", node->name);
+			else
+				printf("declare -x %s\n", node->name);
+		}
+		node = node->next;
+	}
+	ft_env_lst_clear(node, free);
+}
