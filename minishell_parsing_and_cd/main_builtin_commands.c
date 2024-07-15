930e5810c3dba25253231ced452ce542b306df8d
Emma Hedemann <ehedeman@c2r2s6.42wolfsburg.de>
2024-07-15 18:32:00 +0200

    corrected the env sigsev

--- minishell_parsing_and_cd/main_builtin_commands.c
+++ minishell_parsing_and_cd/main_builtin_commands.c
@@ -6,7 +6,7 @@
 /*   By: ehedeman <ehedeman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 16:47:10 by ehedeman          #+#    #+#             */
-/*   Updated: 2024/07/15 17:47:44 by ehedeman         ###   ########.fr       */
+/*   Updated: 2024/07/15 18:30:29 by ehedeman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	check_commands_loop(t_statement *temp, t_mini *mini, int fd, int i)
 			i = check_builtins(temp, mini, i, fd);
 			if (i >= temp->argc)
 				break ;
-			if (!ft_strncmp(temp->argv[i], "./", 2) || !ft_strncmp(temp->argv[i], "/", 1))
+			if (!!ft_strncmp(temp->argv[i], "./", 2) || !!ft_strncmp(temp->argv[i], "/", 1))
 			{
 				if (exec_file(temp, mini) == -1)
 					exit(0);
@@ -47,28 +47,31 @@ int	check_commands_loop(t_statement *temp, t_mini *mini, int fd, int i)
 
 int	check_builtins(t_statement *temp, t_mini *mini, int i, int fd) // needs to return i so other thing knows what word we're at
 {
-	if (ft_strncmp(temp->argv[i], "echo", ft_strlen(temp->argv[i])) == 0)
+	if (!ft_strncmp(temp->argv[i], "echo", ft_strlen(temp->argv[i])))
 	{
 		ft_echo(mini, temp, fd, i);
 		i = temp->argc;
 	}
-	else if (ft_strncmp(temp->argv[i], "cd", ft_strlen(temp->argv[i])) == 0)
+	else if (!ft_strncmp(temp->argv[i], "cd", ft_strlen(temp->argv[i])))
 	{
 		ft_cd(temp, i);
 		i = temp->argc;
 	} //skips the path after use
-	else if (ft_strncmp(temp->argv[i], "pwd", ft_strlen(temp->argv[i])) == 0)
+	else if (!ft_strncmp(temp->argv[i], "pwd", ft_strlen(temp->argv[i])))
 		ft_pwd(fd);
-	else if (ft_strncmp(temp->argv[i], "exit", ft_strlen(temp->argv[i])) == 0)
+	else if (!ft_strncmp(temp->argv[i], "exit", ft_strlen(temp->argv[i])))
 		ft_exit(mini);
-	else if (ft_strncmp(temp->argv[i], "env", ft_strlen(temp->argv[i])) == 0 && !temp->argv[i + 1])
-		return (ft_print_env_lst(mini->env));
-	else if (ft_strncmp(temp->argv[i], "export", ft_strlen(temp->argv[i])) == 0)
+	else if (!ft_strncmp(temp->argv[i], "env", ft_strlen(temp->argv[i])) && !temp->argv[i + 1])
+	{
+		ft_print_env_lst(mini->env);
+		i = temp->argc;
+	}
+	else if (!ft_strncmp(temp->argv[i], "export", ft_strlen(temp->argv[i])))
 	{
 		ft_export(mini);
 		i = temp->argc;
 	}
-	else if (ft_strncmp(temp->argv[i], "unset", ft_strlen(temp->argv[i])) == 0)
+	else if (!ft_strncmp(temp->argv[i], "unset", ft_strlen(temp->argv[i])))
 	{
 		while (temp->argv[i])
 		{
