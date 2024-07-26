4d5eb0d107119ae5a2af9565e5da14370bd51552
smatschu <smatschu@student.42wolfsburg.de>
2024-07-26 08:34:44 +0200

    broke execute_pipeline in smaller functions and added comments to make it easier to understand

--- pipes.c
+++ pipes.c
@@ -6,22 +6,26 @@
 /*   By: smatschu <smatschu@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 21:15:14 by smatschu          #+#    #+#             */
-/*   Updated: 2024/07/19 11:07:40 by smatschu         ###   ########.fr       */
+/*   Updated: 2024/07/26 08:26:38 by smatschu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	create_pipe(int pipefd[])
+// changed to check the command here, will create the pipe if needed.
+void	create_pipes(t_statement *current, int pipefd[])
 {
-	if (pipe(pipefd) == -1)
+	if (current->next && command_involves_pipes(current))
 	{
-		perror("Error: pipe could not be created");
-		exit(EXIT_FAILURE);
+		if (pipe(pipefd) == -1)
+		{
+			perror("Error: pipe could not be created");
+			exit(EXIT_FAILURE);
+		}
 	}
-	return (0);
 }
 
+// redirect standart input to fd
 static int	redirect_stdin(int fd)
 {
 	if (dup2(fd, STDIN_FILENO) == -1)
@@ -30,6 +34,7 @@ static int	redirect_stdin(int fd)
 	return (0);
 }
 
+// redirect standart output to fd
 static int	redirect_stdout(int fd)
 {
 	if (dup2(fd, STDOUT_FILENO) == -1)
@@ -52,61 +57,68 @@ int	command_involves_pipes(t_statement *parsed_input)
 	return (0);
 }
 
+void	child_process(t_statement *curr, t_mini *mini, int in_fd, int pipefd[])
+{
+	if (in_fd != STDIN_FILENO)
+		redirect_stdin(in_fd); // redirect standard input if needed
+	if (curr->next && command_involves_pipes(curr))
+	{
+		close(pipefd[0]); // close the read end of the pipe
+		redirect_stdout(pipefd[1]); // redirect standard output to the pipe
+	}
+	exec_command(curr, mini); // execute the command
+	exit(EXIT_SUCCESS);
+}
+
+void	parent_process(t_statement *current, int *input_fd, int pipefd[])
+{
+	if (*input_fd != STDIN_FILENO)
+		close(*input_fd); // Close the input fd if it's not standard input
+	if (current->next && command_involves_pipes(current))
+	{
+		close(pipefd[1]); // Close the write end of the pipe
+		*input_fd = pipefd[0]; // set the input fd to the read end of the pipe for the next command
+	}
+	else if (!current->next)
+		close(pipefd[0]); // if it's the last command, close the read end of the pipe
+}
+
+// wait for all child processes to finish
+void	wait_for_children(t_mini *mini)
+{
+	int	status;
+
+	while (wait(&status) > 0)
+	{
+		if (WIFEXITED(status))
+			mini->exit_status = WEXITSTATUS(status);
+	}
+}
+
 void	execute_pipeline(t_statement *commands, t_mini *mini)
 {
-	int			pipefd[2];
-	int			input_fd;
-	t_statement	*current;
-	pid_t		pid;
+	int			pipefd[2]; // this is to hold pipe file descriptors
+	int			input_fd; // fd for input
+	t_statement	*current; // pointer to the current command
+	pid_t		pid; // process ID
 
 	input_fd = STDIN_FILENO; // for the first command, input is STDIN
 	current = commands;
 	while (current != NULL)
 	{
-		if (current->next && command_involves_pipes(current))
-		{
-			if (pipe(pipefd) == -1)
-			{
-				perror("pipe");
-				exit(EXIT_FAILURE);
-			}
-		}
-		pid = fork();
+		create_pipes(current, pipefd); // set up pipes if needed
+		pid = fork(); // create a new process
 		if (pid == 0)
-		{	
-			// child process
-			if (input_fd != STDIN_FILENO)
-				redirect_stdin(input_fd);
-			if (current->next && command_involves_pipes(current))
-			{
-				close(pipefd[0]);
-				redirect_stdout(pipefd[1]);
-			}
-			exec_command(current, mini);
-			exit(EXIT_SUCCESS);
-		}
+			child_process(current, mini, input_fd, pipefd);
 		else if (pid < 0)
 		{
 			perror("fork");
 			exit(EXIT_FAILURE);
 		}
-		// parent process
-		if (input_fd != STDIN_FILENO)
-			close(input_fd);
-		if (current->next && command_involves_pipes(current))
-		{
-			close(pipefd[1]);
-			input_fd = pipefd[0];
-		}
-		else if (!current->next)
-			close(pipefd[0]); // if it's the last command, close the read end
-		current = current->next;
+		parent_process(current, &input_fd, pipefd);
+		current = current->next; // go to the next command in the list
 	}
-	// close the last read end if it's not STDIN
 	if (input_fd != STDIN_FILENO)
-		close(input_fd);
-	// wait for all children to finish
-	while (wait(NULL) > 0)
-	{
-	}
+		close(input_fd); // close the last read end if it's not standard input
+	wait_for_children(mini); // wait for all child processes to finish
 }
